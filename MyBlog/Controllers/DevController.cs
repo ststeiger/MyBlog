cbea1ffedac1c3674001407fab0b532f0c55aaf2
ststeiger <quandary82@hailmail.net>
2015-04-29 17:36:08 +0200

    DevController

---
+++ MyBlog/Controllers/DevController.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MyBlog.Controllers
+{
+
+
+    public class DevController : Controller
+    {
+
+
+        public ContentResult Index()
+        {
+            return Info();
+        }
+
+
+        //
+        // GET: /Dev/
+        public ContentResult Info()
+        {
+            string str = @"
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge,chrome=1"" />
+    <meta charset=""utf-8"" />
+    <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"" />
+    <meta http-equiv=""cache-control"" content=""max-age=0"" />
+    <meta http-equiv=""cache-control"" content=""no-cache"" />
+    <meta http-equiv=""expires"" content=""0"" />
+    <meta http-equiv=""expires"" content=""Tue, 01 Jan 1980 1:00:00 GMT"" />
+    <meta http-equiv=""pragma"" content=""no-cache"" />
+
+    <title>NewEntry</title>
+    
+    <script src=""reqwest.js"" charset=""utf-8"" type=""text/javascript""></script>
+    <script src=""ajax_compat.js"" charset=""utf-8"" type=""text/javascript""></script>
+    
+    
+    <style type=""text/css"" media=""all"">
+		
+    </style>
+</head>
+<body>
+	
+<p>{0}</p>
+	
+</body>
+</html>
+
+";
+
+            if(Request.IsLocal)
+                str = string.Format(str, Settings.DAL.GetConnectionString());
+            else
+                str = string.Format(str, "Top Secret");
+            
+            return Content(str, "text/html", System.Text.Encoding.UTF8);
+        }
+
+
+    }
+
+
+}
--- MyBlog/Properties/AssemblyInfo.cs
+++ MyBlog/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2015.4.28.0955")]
-[assembly: AssemblyFileVersion("2015.4.28.0955")]
+[assembly: AssemblyVersion("2015.4.29.1457")]
+[assembly: AssemblyFileVersion("2015.4.29.1457")]
