5c196245d8cec3d00070620717d368706c28600f
ststeiger <quandary82@hailmail.net>
2020-06-15 12:13:32 +0200

    Replace SQL

--- DevTests/Program.cs
+++ DevTests/Program.cs
@@ -35,10 +35,32 @@ namespace DevTests
         public static void foo()
         {
             string fileName = @"C:\Users\username\Documents\Visual Studio 2019\Projects\MyBlog\WikiPlex\Legacy\Colorizer\DefaultStyleSheet.cs";
-            
+
             string content = System.IO.File.ReadAllText(fileName, System.Text.Encoding.UTF8);
 
             string newContent = System.Text.RegularExpressions.Regex.Replace(content, "(Background = \")(.*)(\",)", new System.Text.RegularExpressions.MatchEvaluator(WordScrambler));
+            System.Console.WriteLine(newContent);
+        }
+
+
+
+        public static void ReplaceSQL()
+        {
+            string fileName = @"D:\username\Desktop\Reports\DIN\Alt\01_tfu_RPT_FM_DATA_DIN277_Grob_Raum.sql";
+            fileName = @"D:\username\Desktop\Reports\DIN\Alt\02_tfu_RPT_FM_DATA_DIN277_Grob_Parkplaetze.sql";
+
+            string content = System.IO.File.ReadAllText(fileName, System.Text.Encoding.UTF8);
+
+            string pattern = @"THEN\s+ISNULL\s*\(\s*T_VWS_SVGElement.SVE_areaInSquaremeters\s*--\s*decimal\(12,\s*2\)\s*,ROUND\(\s*T_ZO_AP_Raum_Flaeche.ZO_RMFlaeche_Flaeche\s*,\s*2\)\s*--\s*float\s*\)";
+            string rep = @"THEN ROUND(T_ZO_AP_Raum_Flaeche.ZO_RMFlaeche_Flaeche, 2) -- float ";
+
+            pattern = @"THEN\s+ISNULL\s*\(\s*T_VWS_SVGElement.SVE_areaInSquaremeters\s*--\s*decimal\(12,\s*2\)\s*,ROUND\(\s*T_ZO_AP_Parkplatz_Flaeche.ZO_PPFlaeche_Flaeche\s*,\s*2\)\s*--\s*float\s*\)";
+            rep = @"THEN ROUND(T_ZO_AP_Parkplatz_Flaeche.ZO_PPFlaeche_Flaeche, 2) -- float ";
+
+            string newContent = System.Text.RegularExpressions.Regex.Replace(content, pattern, rep, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            string outputFileName = @"D:\username\Desktop\Reports\DIN\Alt\01a_tfu_RPT_FM_DATA_DIN277_Grob_Raum.sql";
+            outputFileName = @"D:\username\Desktop\Reports\DIN\Alt\02a_tfu_RPT_FM_DATA_DIN277_Grob_Parkplaetze.sql";
+            System.IO.File.WriteAllText(outputFileName, newContent, System.Text.Encoding.UTF8);
             System.Console.WriteLine(newContent);
         }
 
