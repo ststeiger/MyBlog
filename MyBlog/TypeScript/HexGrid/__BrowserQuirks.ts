6300d9e85ae2ced6a15bdacfc6922e667e9a3529
ststeiger <quandary82@hailmail.net>
2020-09-28 17:21:30 +0200

    Added grid

---
+++ MyBlog/TypeScript/HexGrid/__BrowserQuirks.ts
@@ -0,0 +1,19 @@
+
+// declare global{
+/*
+interface Window
+{
+    sqrt(x: number): number;
+    sin(x: number): number;
+    cos(x: number): number;
+
+    PI: number;
+}
+*/
+
+// }
+
+//declare function sqrt(x: number): number;
+//declare function sin(x: number): number;
+//declare function cos(x: number): number;
+//declare let PI:number;
