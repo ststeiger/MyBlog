6af3fd1d10a0341a24d358a3d50382ebb9d85080
Me <Me@stop.spam>
2020-11-18 17:59:22 +0100

    paging for C#

--- _DB/MS/Queries/Query_BlogPosts.sql
+++ _DB/MS/Queries/Query_BlogPosts.sql
@@ -15,6 +15,8 @@ ORDER BY BP_EntryDate DESC
 
 OFFSET 0 ROWS FETCH NEXT 200 ROWS ONLY 
 
+-- "OFFSET " + 0.ToString() + "" ROWS FETCH NEXT " + 200.ToString() + " ROWS ONLY " 
+
 
 -- https://weblog.west-wind.com/posts/2019/Mar/16/ASPNET-Core-Hosting-on-IIS-with-ASPNET-Core-22
 -- https://www.strathweb.com/
